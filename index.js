94e746ba833556b75980783487e0ad06bea5fce2
Aqsa Ishtiaq <aqsaishtiaq19@gmail.com>
2026-07-13 17:40:49 +0500

    Fixed save button to reset counter to zero

--- index.js
+++ index.js
@@ -137,8 +137,11 @@ function increment() {
   count += 1;
   countEl.innerText = count;
 }
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-}
 
+function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+
+    count = 0;              
+    countEl.innerText = count; 
+  }
